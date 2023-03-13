30226e19aa68e048e19b613f591270de064c22bb
GustavoAlves04 <gustavoalcontato@gmail.com>
2023-03-13 17:12:52 -0300

    add-favoritos

--- js/script.js
+++ js/script.js
@@ -8,3 +8,64 @@ const observer = new IntersectionObserver( entries =>{
 
     
 })
+
+const btn = document.getElementById('btn')
+const btn2 = document.getElementById('btn2')
+const btn3 = document.getElementById('btn3')
+const btn4 = document.getElementById('btn4')
+const btn5 = document.getElementById('btn5')
+const btn6 = document.getElementById('btn6')
+
+function Toggle() {
+    if (btn.style.color == 'red') {
+        btn.style.color = 'white'
+    }
+    else{
+        btn.style.color = 'red'
+    }
+}
+
+function Toggle2() {
+    if (btn2.style.color == 'red') {
+        btn2.style.color = 'white'
+    }
+    else{
+        btn2.style.color = 'red'
+    }
+}
+
+function Toggle3() {
+    if (btn3.style.color == 'red') {
+        btn3.style.color = 'white'
+    }
+    else{
+        btn3.style.color = 'red'
+    }
+}
+
+function Toggle4() {
+    if (btn4.style.color == 'red') {
+        btn4.style.color = 'white'
+    }
+    else{
+        btn4.style.color = 'red'
+    }
+}
+
+function Toggle5() {
+    if (btn5.style.color == 'red') {
+        btn5.style.color = 'white'
+    }
+    else{
+        btn5.style.color = 'red'
+    }
+}
+
+function Toggle6() {
+    if (btn6.style.color == 'red') {
+        btn6.style.color = 'white'
+    }
+    else{
+        btn6.style.color = 'red'
+    }
+}
