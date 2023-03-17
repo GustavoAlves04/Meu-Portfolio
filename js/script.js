00e4393339a9a922090cd980de3d6f947b21cd76
GustavoAlves04 <gustavoalcontato@gmail.com>
2023-03-17 17:38:47 -0300

    responsivo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY> 0);
+});
+
+
+const menu = document.querySelector('#menu-icone');
+const navbar = document.querySelector('.navlista');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+
 const observer = new IntersectionObserver( entries =>{
     console.log(entries)
     entries[0].target.classList.add('.init-hidden-off')
